cc4cee10f0e64db8c95ca9b12d634a9c5c96fe7c
mateus <mateus.bellomo@prodatamobility.com.br>
2025-02-26 14:48:56 -0300

    Atualiza configurações para usar schema e dependências do ambiente l2test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM openjdk:17-jdk-slim
+FROM openjdk:21-jdk-slim
 
 WORKDIR /app
 
-COPY target/L2API.jar app.jar
+COPY target/L2WebAPI-0.0.1-SNAPSHOT.jar app.jar
 
 EXPOSE 8080
 
--- src/main/java/com/L2Code/L2WebAPI/model/Contact.java
+++ src/main/java/com/L2Code/L2WebAPI/model/Contact.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "contato", schema = "desafio")
+@Table(name = "contato", schema = "l2test")
 @Getter
 @Setter
 @NoArgsConstructor
